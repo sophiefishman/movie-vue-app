a5b6561c87d93d288e5b3e0fc2ed3685faf47ed7
sophie <sophiefishman@gmail.com>
2021-06-04 09:28:28 -0500

    adds vue crud homework

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2>Create a Movie</h2>
+    Title:
+    <input type="text" v-model="newTitle" />
+    <br />
+    Year:
+    <input type="text" v-model="newYear" />
+    <br />
+    Plot:
+    <input type="text" v-model="newPlot" />
+    <br />
+    <button v-on:click="createMovie()">Add Movie</button>
+    <br />
+    <div v-for="movie in movies" :key="movie.id">
+      <h2>{{ movie.title }}</h2>
+      <button v-on:click="showMovie(movie)">More Info</button>
+      <!-- <p>Year: {{ movie.year }}</p>
+      <p>Plot: {{ movie.plot }}</p> -->
+      <br />
+    </div>
+    <dialog id="movie-details">
+      <form method="dialog">
+        <h1>Movie Info</h1>
+        <img src="" alt="" />
+        <p>Title: {{ currentMovie.title }}</p>
+        <p>Year: {{ currentMovie.year }}</p>
+        <p>Plot: {{ currentMovie.plot }}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      movies: [],
+      newTitle: "",
+      newYear: "",
+      newPlot: "",
+      currentMovie: {},
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/movies").then((response) => {
+        console.log(response.data);
+        this.movies = response.data;
+      });
+    },
+    createMovie: function () {
+      var params = {
+        title: this.newTitle,
+        year: this.newYear,
+        plot: this.newPlot,
+      };
+      axios.post("http://localhost:3000/movies", params).then((response) => {
+        this.movies.push(response.data);
+        console.log(response.data);
+      });
+    },
+    showMovie: function (movie) {
+      console.log(movie);
+      this.currentMovie = movie;
+      document.querySelector("#movie-details").showModal();
+    },
   },
 };
 </script>
