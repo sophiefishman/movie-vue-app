f54c2f1afa9c37e112b28423f81eda192a137b4e
sophie <sophiefishman@gmail.com>
2021-06-15 10:44:39 -0500

    adds vue extras

--- src/views/Actors/Index.vue
+++ src/views/Actors/Index.vue
@@ -2,6 +2,7 @@
   <div class="actors-index">
     <h2>Actors</h2>
     <div v-for="actor in actors" v-bind:key="actor.id">
+      <div v-for="actor in filterBy(actors, searchTerm, 'full-name')" v-bind:key="actor.id"></div>
       <router-link :to="`/actors/${actor.id}`">
         <h4>{{ actor.first_name }} {{ actor.last_name }}</h4>
       </router-link>
@@ -13,9 +14,12 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 export default {
+  mixins: [Vue2Filters.mixin],
   data: () => ({
     actors: [],
+    searchTerm: "",
   }),
   created: function () {
     axios.get("/actors").then((response) => {
--- src/views/Movies/Index.vue
+++ src/views/Movies/Index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="movies-index">
     <h2>Movies</h2>
-    <div class="movie" v-for="movie in movies" v-bind:key="movie.id">
+    <input type="text" v-model="titleFilter" />
+    <div class="movie" v-for="movie in filterBy(movies, titleFilter, 'title')" v-bind:key="movie.id">
       <router-link :to="`/movies/${movie.id}`">
         <h4>{{ movie.title }} ({{ movie.year }})</h4>
       </router-link>
@@ -11,9 +12,12 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 export default {
+  mixins: [Vue2Filters.mixin],
   data: () => ({
     movies: [],
+    titleFilter: "",
   }),
   created: function () {
     axios.get("/movies").then((response) => {
